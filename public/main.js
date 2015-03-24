49247e7a929ff12c31d80be28a8899146811e1d6
David Jones <daverival@hotmail.com>
2015-03-24 13:07:37 +0000

    Added locked rooms

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 var socket = io.connect();
+var cachedRooms = [];
 
 socket.on('connect', function(){
     socket.emit('adduser', prompt("What's your name: "));
@@ -8,21 +9,30 @@ socket.on('updatechat', function (username, data) {
     $('#conversation').append('<li class="list-group-item"><b>'+ username + ':</b> ' + data + '</li>');
 });
 
-
 socket.on('updaterooms', function (rooms, current_room) {
     $('#rooms').empty();
+    cachedRooms = rooms;
+    console.log(cachedRooms);
     $.each(rooms, function(key, value) {
-        if(value == current_room){
-            $('#rooms').append('<li role="presentation" class="active"><a href="#">' + value + '</a></li>');
+    	var icon = '';
+    	if(value.requiresPassword ){
+    		icon = '<i class="glyphicon glyphicon-lock"></i> ';
+    	}
+        if(value.name == current_room){
+            $('#rooms').append('<li role="presentation" class="active"><a href="#">' + icon + value.name + '</a></li>');
         }
         else {
-            $('#rooms').append('<li role="presentation"><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></lit>');
+            $('#rooms').append('<li role="presentation"><a href="#" onclick="switchRoom(\''+value.name+'\')">' + icon + value.name + '</a></lit>');
         }
     });
 });
 
-function switchRoom(room){
-    socket.emit('switchRoom', room);
+function switchRoom(room){	
+	var password ='';
+	if(cachedRooms[room].requiresPassword){
+		password = prompt('Please enter password');	
+	}
+    socket.emit('switchRoom',{name: room, password: password});
 }
 
 $(function(){
--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 var express = require('express')
-		, app = express()
+	, app = express()
   	, server = require('http').createServer(app)
   	, io = require('socket.io').listen(server)
-		, sanitizer = require('sanitizer');
+	, sanitizer = require('sanitizer');
 
 var logger = require('./app/logger');
 var usernames = {};
-var rooms = ['Lobby','Room A','Room B'];
+var rooms = {
+    'Lobby' : {
+        name : 'Lobby',
+        requiresPassword: false,
+        password : ''
+    },
+    'Open Room' : {
+        name : 'Open Room',
+        requiresPassword: false,
+        password : ''
+    },
+    'Locked Room' : {
+        name : 'Locked Room',
+        requiresPassword: true,
+        password : 'password'
+    }
+};
 
 app.use(logger);
 app.use(express.static('public'));
-
 app.get('/',function(request,response){
 	response.sendFile(__dirname + '/public/index.html');
-});
-
-app.get('/data', function(request,response){
-	response.send(['London','Brighton','Canterbury']);
-	response.end();
 });
 
 io.sockets.on('connection', function(socket) {
@@ -27,29 +37,33 @@ io.sockets.on('connection', function(socket) {
         usernames[username] = username;
         socket.join('Lobby');
         socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
-        socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected to this room');
-        socket.emit('updaterooms', rooms, 'Lobby');
+        socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', username + ' has connected to this room');       
+        socket.emit('updaterooms', makeRoomsSafeToSend(rooms), 'Lobby');
     });
 
-    socket.on('create', function(room) {
+    /*socket.on('create', function(room) {
         //rooms.push(room);
         socket.emit('updaterooms', rooms, socket.room);
-    });
+    });*/
 
     socket.on('sendchat', function(data) {
         io.sockets["in"](socket.room).emit('updatechat', socket.username, data);
     });
 
     socket.on('switchRoom', function(newroom) {
-        var oldroom;
-        oldroom = socket.room;
-        socket.leave(socket.room);
-        socket.join(newroom);
-        socket.emit('updatechat', 'SERVER', 'you have connected to ' + newroom);
-        socket.broadcast.to(oldroom).emit('updatechat', 'SERVER', socket.username + ' has left this room');
-        socket.room = newroom;
-        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username + ' has joined this room');
-        socket.emit('updaterooms', rooms, newroom);
+        if(rooms[newroom.name].password == newroom.password){
+            var oldroom;
+            oldroom = socket.room;
+            socket.leave(socket.room);
+            socket.join(newroom.name);
+            socket.emit('updatechat', 'SERVER', 'you have connected to ' + newroom.name);
+            socket.broadcast.to(oldroom).emit('updatechat', 'SERVER', socket.username + ' has left this room');
+            socket.room = newroom.name;
+            socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username + ' has joined this room');
+            socket.emit('updaterooms', rooms, newroom.name);
+        } else {
+            socket.emit('updatechat', 'SERVER', 'you cant connect to that room. Incorrect password.');
+        }
     });
 
     socket.on('disconnect', function() {
@@ -60,6 +74,14 @@ io.sockets.on('connection', function(socket) {
     });
  });
 
+function makeRoomsSafeToSend(rooms){
+    var safeToSendRooms = {};
+    Object.keys(rooms).forEach(function(key){
+        var item = rooms[key];
+        safeToSendRooms[item.name] = {name:item.name,requiresPassword:item.requiresPassword};
+    });
+    return safeToSendRooms
+}
 
 app.start = app.listen = function(){
   return server.listen.apply(server, arguments)

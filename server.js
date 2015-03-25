a865e180d9ca6e93db4f63150429543ec434aaac
ziphrax <daverival@hotmail.com>
2015-03-25 18:52:02 +0000

    Moved to port 80 for live website

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express')
 
 var logger = require('./app/logger');
 var usernames = {};
-var port = process.env.PORT || 5000
+var port = process.env.PORT || 80;
 var rooms = {
     'Lobby' : {
         name : 'Lobby',
@@ -30,28 +30,28 @@ app.use(logger);
 app.use(express.static('public'));
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.get('/',function(request,response){
-	response.sendFile(__dirname + '/public/index.html');    
+	response.sendFile(__dirname + '/public/index.html');
 });
 app.get('/terms-of-service',function(request,response){
-	response.sendFile(__dirname + '/public/terms.html');    
+	response.sendFile(__dirname + '/public/terms.html');
 });
 
 app.get('/news',function(request,response){
-	response.sendFile(__dirname + '/public/news.html');    
+	response.sendFile(__dirname + '/public/news.html');
 });
 
 app.get('/users',function(request,response){
-    response.sendFile(__dirname + '/public/users.html');        
+    response.sendFile(__dirname + '/public/users.html');
 });
 
-app.get('/data/users',function(request,response){    
-    response.json(usernames);    
+app.get('/data/users',function(request,response){
+    response.json(usernames);
     response.end();
 });
 
 io.sockets.on('connection', function(socket) {
 
-    socket.emit('usercount',io.sockets.sockets.length);  
+    socket.emit('usercount',io.sockets.sockets.length);
 
     socket.on('adduser', function(username) {
         if(validateUsername(username)){
@@ -61,12 +61,12 @@ io.sockets.on('connection', function(socket) {
             socket.join('Lobby');
             socket.emit('updatechat', 'SERVER', 'you have connected to Lobby');
             socket.broadcast.to('Lobby').emit('updatechat', 'SERVER', socket.username + ' has connected to this room');
-            socket.emit('updaterooms', makeRoomsSafeToSend(rooms), 'Lobby');    
-            socket.broadcast.emit('usercount',io.sockets.sockets.length);  
+            socket.emit('updaterooms', makeRoomsSafeToSend(rooms), 'Lobby');
+            socket.broadcast.emit('usercount',io.sockets.sockets.length);
         } else {
             socket.emit('updatechat','SERVER','username already in use');
             socket.disconnect();
-        }        
+        }
     });
 
     /*socket.on('create', function(room) {
@@ -85,11 +85,11 @@ io.sockets.on('connection', function(socket) {
             socket.leave(socket.room);
             socket.join(newroom.name);
             socket.emit('updatechat', 'SERVER', 'you have connected to ' + newroom.name);
-            socket.broadcast.to(oldroom).emit('updatechat', 'SERVER', socket.username + ' has left this room');            
+            socket.broadcast.to(oldroom).emit('updatechat', 'SERVER', socket.username + ' has left this room');
             socket.room = newroom.name;
             socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username + ' has joined this room');
-            socket.broadcast.emit('usercount',io.sockets.sockets.length);        
-            socket.emit('updaterooms', makeRoomsSafeToSend(rooms), newroom.name);            
+            socket.broadcast.emit('usercount',io.sockets.sockets.length);
+            socket.emit('updaterooms', makeRoomsSafeToSend(rooms), newroom.name);
         } else {
             socket.emit('updatechat', 'SERVER', 'you cant connect to that room. Incorrect password.');
         }
@@ -99,7 +99,7 @@ io.sockets.on('connection', function(socket) {
         delete usernames[socket.username];
         io.sockets.emit('updateusers', usernames);
         socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-        socket.broadcast.emit('usercount',io.sockets.sockets.length);               
+        socket.broadcast.emit('usercount',io.sockets.sockets.length);
         socket.leave(socket.room);
     });
  });

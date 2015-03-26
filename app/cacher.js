fc264509ae190c88641836e96f9cd446bfb04bcb
David Jones <daverival@hotmail.com>
2015-03-26 13:18:53 +0000

    Added cache headers

--- app/cacher.js
+++ app/cacher.js
@@ -1,5 +1,5 @@
 module.exports = function cacher(request,response,next){
-	res.header('Cache-Control', 'max-age=120');
-	res.header('ETag','x234dff');
+	response.header('Cache-Control', 'max-age=120');
+	response.header('ETag','x234dff');
 	next();
 }

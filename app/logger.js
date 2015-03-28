726d14b7f0c2b087c9dd0986f934573d70cc72cb
ziphrax <daverival@hotmail.com>
2015-03-28 03:08:32 +0000

    Changed to use modal dialogs for user input

--- app/logger.js
+++ app/logger.js
@@ -1,3 +1,12 @@
+var mongoose = require('mongoose');
+var LogSchema = new mongoose.Schema({
+	time: Date,
+	message: String,
+	ip: String
+});
+
+var Log = mongoose.model('Log',LogSchema);
+
 module.exports = function logger(request,response,next){
 	var start = +new Date();
 	var stream = process.stdout;
@@ -7,8 +16,17 @@ module.exports = function logger(request,response,next){
 
 	response.on('finish', function(){
 		var duration = +new Date() -  start;
-		var message = ip + ': '+ method + ' ' + url + ' ' + duration + ' ms \n';
-		stream.write(message);
+		var message =   method + ' ' + url + ' ' + duration + ' ms';
+		var log = new Log({
+			time:new Date(),
+			ip:ip,
+			message:message
+		}).save(function(err){
+			if(err){
+				console.err(err);
+			}
+		});
+		stream.write(ip + ': '+ message + ' \n');
 	});
 
 	next();
--- app/main.js
+++ app/main.js
@@ -1,76 +1,67 @@
-var socket = io.connect();
-var cachedRooms = [];
-
-socket.on('connect', function(){
-    socket.emit('adduser', prompt("What's your name: "));
-});
-
-socket.on('updatechat', function (username, data) {
-    $('#conversation').append('<li class="list-group-item"><b>'+ username + ': ' + formatedTime() +'-></b> ' + data + '</li>');
-    scrollConversation();
-});
-
-socket.on('updaterooms', function (rooms, current_room) {
-    $('#rooms').empty();
-    cachedRooms = rooms;
-    $.each(rooms, function(key, value) {
-    	var icon = '';
-    	if(value.requiresPassword && value.name == current_room){
-    		icon = '<i class="glyphicon glyphicon-ok"></i> ';
-    	} else if (value.requiresPassword){
-    		icon = '<i class="glyphicon glyphicon-lock"></i> ';
-    	}
-        if(value.name == current_room){
-            $('#rooms').append('<li role="presentation" class="active"><a href="#">' + icon + value.name + '</a></li>');
-        }
-        else {
-            $('#rooms').append('<li role="presentation"><a href="#" onclick="switchRoom(\''+value.name+'\')">' + icon + value.name + '</a></lit>');
-        }        
-    });
-});
-
-socket.on('usercount',function(count){
-  $('#usercount').text(count);
-});
-
-socket.on('error',function(error){
-  $('#conversation').append('<li class="list-group-item"><b>SERVER: ' + formatedTime() +'-></b> Whoops something went wrong' + error + '</li>');
-  scrollConversation();
-});
-
-socket.on('disconnect',function(){
-  $('#conversation').append('<li class="list-group-item"><b>SERVER: ' + formatedTime() +'-></b> Lost connection to the server :-(</li>');
-  scrollConversation();
-});
-
-function scrollConversation(){
-  var objDiv = document.getElementById("conversation");
-  objDiv.scrollTop = objDiv.scrollHeight;
-}
-
-function formatedTime(){
-  var d = new Date();
-  return  d.getHours() + ':' +
-  d.getMinutes() + ':' +
-  d.getSeconds();
-}
-
-function switchRoom(room){
-	var password ='';
-	if(cachedRooms[room].requiresPassword){
-		password = prompt('Please enter password');
-	}
-    socket.emit('switchRoom',{name: room, password: password});
-}
-
+var socket;
 $(function(){
-    $('#datasend').click( function(e) {
-        e.preventDefault();
-        var message = $('#data').val();
-        $('#data').val('');
-        $('#data').focus();
-        socket.emit('sendchat', message);
+
+    $('#join').click(function(e){
+      io.connect();
+      e.preventDefault();
+      socket = io.connect();;
+      var cachedRooms = [];
+
+      socket.on('connect', function(){
+          var username = $('#username').val();
+          socket.emit('adduser', username);
+          $('.disconnected').fadeOut(500,function(){
+            $('.connected').fadeIn();
+          });
+      });
+
+      socket.on('updatechat', function (username, data) {
+          $('#conversation').append('<li class="list-group-item"><b>'+ username + ': ' + formatedTime() +'-></b> ' + data + '</li>');
+          scrollConversation();
+      });
+
+      socket.on('updaterooms', function (rooms, current_room) {
+          $('#rooms').empty();
+          cachedRooms = rooms;
+          $.each(rooms, function(key, value) {
+            var icon = '';
+            if(value.requiresPassword && value.name == current_room){
+              icon = '<i class="glyphicon glyphicon-ok"></i> ';
+            } else if (value.requiresPassword){
+              icon = '<i class="glyphicon glyphicon-lock"></i> ';
+            }
+              if(value.name == current_room){
+                  $('#rooms').append('<li role="presentation" class="active"><a href="#" data-room="'+value.name+'">'+ icon + value.name + '</a></li>');
+              }
+              else {
+                  $('#rooms').append('<li role="presentation"><a href="#" class="inactive" data-room=\''+value.name+'\'">' + icon + value.name + '</a></lit>');
+              }
+              $('#rooms').find('li a.inactive').on('click',function(e){
+                var room = $(this).data('room');
+                var password ='';
+                if(cachedRooms[room].requiresPassword){
+                  password = prompt('Please enter password');
+                }
+                  socket.emit('switchRoom',{name: room, password: password});
+              });
+          });
+      });
+
+      socket.on('usercount',function(count){
+        $('#usercount').text(count);
+      });
+
+      socket.on('error',function(error){
+        $('#conversation').append('<li class="list-group-item"><b>SERVER: ' + formatedTime() +'-></b> Whoops something went wrong' + error + '</li>');
+        scrollConversation();
+      });
+
+      socket.on('disconnect',function(){
+        $('#conversation').append('<li class="list-group-item"><b>SERVER: ' + formatedTime() +'-></b> Lost connection to the server :-(</li>');
+        scrollConversation();
+      });
     });
+
 
     $('#data').keypress(function(e) {
         if(e.which == 13) {
@@ -80,9 +71,31 @@ $(function(){
         }
     });
 
+    function scrollConversation(){
+      var objDiv = document.getElementById("conversation");
+      objDiv.scrollTop = objDiv.scrollHeight;
+    }
+
+    function formatedTime(){
+      var d = new Date();
+      return  d.getHours() + ':' +
+      d.getMinutes() + ':' +
+      d.getSeconds();
+    }
+
+    $('#datasend').click( function(e) {
+        e.preventDefault();
+        var message = $('#data').val();
+        $('#data').val('');
+        $('#data').focus();
+        socket.emit('sendchat', message);
+    });
+
     $('#createroom').click(function(){
-        var roomName = prompt("New Room name?");
-        var password = prompt("Password for the room?");
+        var roomName = $('#roomname').val();
+        var password = $('#roompassword').val();
         socket.emit('create', roomName,password);
     });
+
+
 });

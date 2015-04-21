cf43ae840afb4921b8c543c768af4f5f586f795d
ziphrax <daverival@hotmail.com>
2015-04-21 02:21:05 +0100

    Improved data routes

--- routes/data.js
+++ routes/data.js
@@ -7,75 +7,70 @@ var Log = require('../model/log');
 var ChatLog = require('../model/chatLog');
 
 router.use('/notes',notes);
-
-router.route('/logs/dailyhitrate/:token')
+router.route('/logs/dailyhitrate/')
   .get(auth,function(request,response){
-    if(request.params.token == '241085.0129'){
-        var options = {};
-        options.map = function(){
-            var d = this.time;
-            d.setHours(0);
-            d.setMinutes(0);
-            d.setSeconds(0);
-            d.setMilliseconds(0);
-            emit(d,this.ip);
-        }
+      var options = {};
+      options.map = function(){
+          var d = new Date();
+          d.setHours(0);
+          d.setMinutes(0);
+          d.setSeconds(0);
+          d.setMilliseconds(0);
+          emit(d,this.ip);
+      }
 
-        options.reduce = function(key,values){
-            var sum = 0;
-            for(var v in values){
-                sum += 1;
-            }
-            return sum;
-        };
+      options.reduce = function(key,values){
+          var sum = 0;
+          for(var v in values){
+              sum += 1;
+          }
+          return sum;
+      };
 
-        Log.mapReduce(options,function(err,results,stats){
-            if(err){
-                console.log(err);
-                response.status(500).send(err);
-            } else {
-                console.log("hitrate map reduce took %d ms", stats.processtime);
-                response.json(results);
-            }
-        });
-    } else {
-        response.status(401).send('Unauthorized');
-    }
+      Log.mapReduce(options,function(err,results,stats){
+          if(err){
+              console.log(err);
+              response.status(500).send(err);
+          } else {
+              console.log("hitrate map reduce took %d ms", stats.processtime);
+              response.json(results);
+          }
+      });
 });
-
-router.route('/logs/raw/:token')
+router.route('/logs/')
   .get(auth,function(request,response){
-    if(request.params.token == '241085.0129'){
-        Log.find().exec(function(err,docs){
-            if(err){
-                console.log(err);
-                response.status(500).send(err);
-            } else {
-                response.json(docs);
-                response.end();
-            }
-        });
-    } else {
-        response.status(401).send('Unauthorized');
-    }
+      Log.find().exec(function(err,docs){
+          if(err){
+              console.log(err);
+              response.status(500).send(err);
+          } else {
+              response.json(docs);
+              response.end();
+          }
+      });
 });
-
-router.route('/chatlogs/:token')
+router.route('/chatlogs/')
   .get(auth,function(request,response){
-    if(request.params.token == '241085.0129'){
-        ChatLog.find().exec(function(err,docs){
-            if(err){
-                console.log(err);
-                response.status(500).send(err);
-            } else {
-                response.json(docs);
-                response.end();
-            }
-        });
-    } else {
-        response.status(401).send('Unauthorized');
-    }
-
+      ChatLog.find().exec(function(err,docs){
+          if(err){
+              console.log(err);
+              response.status(500).send(err);
+          } else {
+              response.json(docs);
+              response.end();
+          }
+      });
 });
-
+router.route('/chatlogs/:username')
+  .get(auth,function(request,response){
+      ChatLog.find({username:request.params.username}).exec(function(err,docs){
+          if(err){
+              console.log(err);
+              response.status(500).send(err);
+          } else {
+              response.json(docs);
+              response.end();
+          }
+      });
+});
 module.exports = router;

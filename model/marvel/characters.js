3e3511ffdbc2118470530e93813de49b3ca4172a
David Jones <daverival@hotmail.com>
2015-05-13 09:55:08 +0100

    Added Characters to marvel pages

---
+++ model/marvel/characters.js
@@ -0,0 +1,28 @@
+var request = require('request');
+var md5 = require('MD5');
+var marvelAPI = {
+    publicKey : 'ed27ed50f19f2ab2c44098a2ec18e8cb',
+    privateKey : '249195ecfb9452872b1dcbce0deaf302b9bbe060'
+};
+var baseURL = 'http://gateway.marvel.com/';
+var namespace = 'v1/public/';
+
+function hash(ts)
+{
+	return md5.digest_s(ts + marvelAPI.privateKey + marvelAPI.publicKey);
+}
+
+module.exports.find = function(character_id,offset,search,callback){
+	var ts = +new Date();
+	var url = '';
+  
+	if(character_id){
+		url = baseURL + namespace + 'characters/' + character_id + '?ts=' + ts + '&apikey=' + marvelAPI.publicKey + '&hash=' + hash(ts);
+	} else {
+		url = baseURL + namespace + 'characters?ts=' + ts + '&offset=' + offset + '&apikey=' + marvelAPI.publicKey + '&hash=' + hash(ts);// + (search?'&titleStartsWith=' + search:'');
+	}
+
+	request( url ,function(err,res,body){
+		callback(err,res,body,search); // Show the HTML for the Google homepage.
+	});
+}
--- routes/marvel.js
+++ routes/marvel.js
@@ -1,69 +1,135 @@
 var express = require('express');
 var router = express.Router();
 var comics = require('../model/marvel/comics');
+var characters = require('../model/marvel/characters');
 var request = require('request');
 
-router.route('/data/comics/')
-	.get(function(req,res){
-		comics.find(null,function(err,response,body){
-			if(err){
-				console.log(err)
-			} else {
-				res.json(JSON.parse(body));
-			}
-		});
-	});
-
-router.route('/data/comics/:id')
-	.get(function(req,res){
-		comics.find(req.params.id,function(err,response,body){
-			if(err){
-				console.log(err)
-			} else {
-				res.json(JSON.parse(body));
-			}
-		});
-	});
-
-router.route('/comics/')
-	.get(function(req,res){
-		comics.find(null,0,'',function(err,response,body){
-			if(err){
-				console.log(err)
-			} else {
-				res.render('pages/marvel/comics',{comics: JSON.parse(body),search: ''});
-			}
-		});
-	});
-router.route('/comics/offset/:offset')
-	.get(function(req,res){
-		comics.find(null,req.params.offset,'',function(err,response,body){
-			if(err){
-				console.log(err)
-			} else {
-				res.render('pages/marvel/comics',{comics: JSON.parse(body),search: ''});
-			}
-		});
-	});
-	router.route('/comics/offset/:offset/search/:search')
+//comics
+	router.route('/data/comics/')
 		.get(function(req,res){
-			comics.find(null,req.params.offset,req.params.search,function(err,response,body,search){
+			comics.find(null,function(err,response,body){
 				if(err){
 					console.log(err)
 				} else {
-					res.render('pages/marvel/comics',{comics: JSON.parse(body),search:search?search:''});
+					res.json(JSON.parse(body));
 				}
 			});
 		});
-router.route('/comics/:id/')
-	.get(function(req,res){
-		comics.find(req.params.id,null,null,function(err,response,body){
-			if(err){
-				console.log(err)
-			} else {
-				res.render('pages/marvel/comic',{comics: JSON.parse(body),search: '' });
-			}
-		});
-	});
 
+	router.route('/data/comics/:id')
+		.get(function(req,res){
+			comics.find(req.params.id,function(err,response,body){
+				if(err){
+					console.log(err)
+				} else {
+					res.json(JSON.parse(body));
+				}
+			});
+		});
+
+	router.route('/comics/')
+		.get(function(req,res){
+			comics.find(null,0,'',function(err,response,body){
+				if(err){
+					console.log(err)
+				} else {
+					res.render('pages/marvel/comics',{comics: JSON.parse(body),search: ''});
+				}
+			});
+		});
+	router.route('/comics/offset/:offset')
+		.get(function(req,res){
+			comics.find(null,req.params.offset,'',function(err,response,body){
+				if(err){
+					console.log(err)
+				} else {
+					res.render('pages/marvel/comics',{comics: JSON.parse(body),search: ''});
+				}
+			});
+		});
+		router.route('/comics/offset/:offset/search/:search')
+			.get(function(req,res){
+				comics.find(null,req.params.offset,req.params.search,function(err,response,body,search){
+					if(err){
+						console.log(err)
+					} else {
+						res.render('pages/marvel/comics',{comics: JSON.parse(body),search:search?search:''});
+					}
+				});
+			});
+	router.route('/comics/:id/')
+		.get(function(req,res){
+			comics.find(req.params.id,null,null,function(err,response,body){
+				if(err){
+					console.log(err)
+				} else {
+					res.render('pages/marvel/comic',{comics: JSON.parse(body),search: '' });
+				}
+			});
+		});
+
+
+//characters
+	router.route('/data/characters/')
+		.get(function(req,res){
+			characters.find(null,function(err,response,body){
+				if(err){
+					console.log(err)
+				} else {
+					res.json(JSON.parse(body));
+				}
+			});
+		});
+
+	router.route('/data/characters/:id')
+		.get(function(req,res){
+			characters.find(req.params.id,function(err,response,body){
+				if(err){
+					console.log(err)
+				} else {
+					res.json(JSON.parse(body));
+				}
+			});
+		});
+
+	router.route('/characters/')
+		.get(function(req,res){
+			characters.find(null,0,'',function(err,response,body){
+				if(err){
+					console.log(err)
+				} else {
+					res.render('pages/marvel/characters',{characters: JSON.parse(body),search: ''});
+				}
+			});
+		});
+	router.route('/characters/offset/:offset')
+		.get(function(req,res){
+			characters.find(null,req.params.offset,'',function(err,response,body){
+				if(err){
+					console.log(err)
+				} else {
+					res.render('pages/marvel/characters',{characters: JSON.parse(body),search: ''});
+				}
+			});
+		});
+		router.route('/characters/offset/:offset/search/:search')
+			.get(function(req,res){
+				characters.find(null,req.params.offset,req.params.search,function(err,response,body,search){
+					if(err){
+						console.log(err)
+					} else {
+						res.render('pages/marvel/characters',{characters: JSON.parse(body),search:search?search:''});
+					}
+				});
+			});
+	router.route('/characters/:id/')
+		.get(function(req,res){
+			characters.find(req.params.id,null,null,function(err,response,body){
+				if(err){
+					console.log(err)
+				} else {
+					res.render('pages/marvel/character',{characters: JSON.parse(body),search: '' });
+				}
+			});
+		});
 module.exports = router

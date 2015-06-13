0ef754b5c6c78d9346d977f6bf5d31bb122fefc1
ziphrax <daverival@hotmail.com>
2015-06-13 03:14:07 +0100

    Basic  xenos list and create functionality added;

--- routes/miniGame.js
+++ routes/miniGame.js
@@ -37,7 +37,7 @@ var request = require('request');
 				}
 			});
 		});
-	router.route('/weapons/:id/')
+	router.route('/weapons/:id')
 		.get(function(req,res){
       weapons.findOne({_id: req.params.id},function(err,docs){
 				if(err){
@@ -81,16 +81,55 @@ var request = require('request');
 					res.render('pages/miniGame/xenos',{xenos: docs});
 				}
 			});
-		});
+		}).post(function(req,res){
+	      var xeno = new xenos();
 
-	router.route('/xenos/:id/')
+				xeno.title = req.body.title;
+				xeno.description = req.body.description;
+				xeno.faction = req.body.faction;
+				xeno.strength = req.body.strength;
+				xeno.toughness = req.body.toughness;
+				xeno.level = req.body.level;
+
+				xeno.save(function(err){
+	        if(err) {
+	          res.status(500).send(err);
+	        } else {
+						res.redirect('/minigame/xenos/' + xeno._id);
+	        }
+	      });
+	  });
+
+	router.route('/xenos/:id')
 		.get(function(req,res){
       xenos.findOne({_id: req.params.id},function(err,docs){
 				if(err){
 					console.log(err)
 				} else {
-					res.render('pages/miniGame/xeno',{xenos: docs});
+					res.render('pages/miniGame/xeno',{xeno: docs});
 				}
 			});
-		});
+		}).post(function(req,res){
+			xenos.findOne({_id: req.params.id},function(err,doc){
+				if(err){
+					console.log(err)
+				} else {
+
+					doc.title = req.body.title;
+					doc.description = req.body.description;
+					doc.faction = req.body.faction;
+					doc.strength = req.body.strength;
+					doc.toughness = req.body.toughness;
+					doc.level = req.body.level;
+
+					doc.save(function(err){
+						if(err) {
+							res.status(500).send(err);
+						} else {
+							res.redirect('/minigame/xenos/' + doc._id);
+						}
+					});
+				}
+			});
+	  });
 module.exports = router
